77c3dfe548a08c50569476925c91fc81a96a2e08
Anton Tivonenko <anton.tivonenko@gmail.com>
2025-02-12 19:49:26 +0300

    Файлы локализации

---
+++ lang/text-en_GB.php
@@ -0,0 +1,65 @@
+<?php
+/**
+ * Этот файл является частью расширения модуля веб-приложения GearMagic.
+ * 
+ * Пакет английской (британской) локализации.
+ * 
+ * @link https://gearmagic.ru
+ * @copyright Copyright (c) 2015 Веб-студия GearMagic
+ * @license https://gearmagic.ru/license/
+ */
+
+return [
+    '{name}'        => 'Media folder structure',
+    '{description}' => 'Organizing media folders of materials into a structure',
+    '{permissions}' => [
+        'any'  => ['Full access', 'Full access to the media folder structure']
+    ],
+
+    // Grid: фильтр
+    'Root media folders' => 'Root media folders',
+    // Grid: контекстное меню записи
+    'Edit media folder' => 'Edit media folder',
+    'Add to media folder' => 'Add to media folder',
+    'Browse' => 'Browse',
+    // Grid: панель инструментов
+    'Adding a new media folder' => 'Adding a new media folder',
+    'Deleting selected media folders' => 'Deleting selected media folders',
+    'Delete all media folders' => 'Delete all media folders',
+    'Select all media fodlers in a list' => 'Select all media fodlers in a list',
+    'Profiling an media folder' => 'Profiling an media folder',
+    // Grid: столбцы
+    'Index' => 'Index',
+    'Index number' => 'Index number',
+    'Name' => 'Name',
+    'Media folder name' => 'Media folder name',
+    'Profile' => 'Profile',
+    'Folder profile name' => 'Folder profile name',
+    'Path' => 'Path',
+    'Local path to a directory (folder)' => 'Local path to a directory (folder)',
+    'Alias' => 'Alias',
+    'Breadcrumb' => 'Breadcrumb',
+    'Breadcrumb element title' => 'Breadcrumb element title',
+    'Icon' => 'Icon',
+    'Parent alias' => 'Parent alias',
+    'Number of child media folders' => 'Number of child media folders',
+    'Visible' => 'Visible',
+    'Show / hide media folders' => 'Show / hide media folders',
+    // Grid: сообщения / заголовки
+    'Show' => 'Show',
+    'Hide' => 'Hide',
+    // Grid: сообщения / текст
+    'Media folder - hide' => 'Media folder - <b>hide</b>.',
+    'Media folder - show' => 'Media folder - <b>show</b>.',
+    // Grid: аудит
+    'media folder with id {0} is hidden' => 'media folder with id "<b>{0}</b>" is hidden',
+    'media folder with id {0} is shown' => 'media folder with id "<b>{0}</b>" is shown',
+
+    // Form
+    '{form.title}' => 'Add media folder',
+    '{form.appendTo}' => 'Add media folder to "{name}"',
+    '{form.titleTpl}' => 'Edit media folder "{name}"',
+    // Form: поля
+    'An alias always begins with an "@" symbol' => 'An alias always begins with an "@" symbol', 
+    'Folder ID, for example: public/uploads' => 'Folder ID, for example: public/uploads'
+];
---
+++ lang/text-ru_RU.php
@@ -0,0 +1,68 @@
+<?php
+/**
+ * Этот файл является частью расширения модуля веб-приложения GearMagic.
+ * 
+ * Пакет русской локализации.
+ * 
+ * @link https://gearmagic.ru
+ * @copyright Copyright (c) 2015 Веб-студия GearMagic
+ * @license https://gearmagic.ru/license/
+ */
+
+return [
+    '{name}'        => 'Структура медиапапок',
+    '{description}' => 'Организация медиапапок материалов в структуру',
+    '{permissions}' => [
+        'any'  => ['Полный доступ', 'Полный доступ к структуре медиапапок']
+    ],
+
+    // Grid: фильтр
+    'Root media folders' => 'Корневые медиапапки',
+    // Grid: контекстное меню записи
+    'Edit media folder' => 'Редактировать',
+    'Add to media folder' => 'Добавить в медиапапку',
+    'Browse' => 'Просмотреть',
+    // Grid: панель инструментов
+    'Adding a new media folder' => 'Добавление новой медиапапки',
+    'Deleting selected media folders' => 'Удаление выделенных медиапапок',
+    'Delete all media folders' => 'Удаление всех медиапапок',
+    'Select all media fodlers in a list' => 'Выделение всех медиапапок в списке',
+    'Profiling an media folder' => 'Профилирование медиапапки',
+    // Grid: столбцы
+    'Index' => 'Порядок',
+    'Index number' => 'Порядковый номер',
+    'Name' => 'Название',
+    'Media folder name' => 'Название медиапапки',
+    'Profile' => 'Профиль',
+    'Folder profile name' => 'Название профиля папки',
+    'Path' => 'Путь',
+    'Local path to a directory (folder)' => 'Локальный путь к каталогу (папке)',
+    'Alias' => 'Псевдоним',
+    'Breadcrumb' => 'Заголовок',
+    'Breadcrumb element title' => 'Заголовок элемента в навигационной цепочке',
+    'Icon' => 'Значок',
+    'Parent alias' => 'Родительский пcевдоним',
+    'Number of child media folders' => 'Количество вложенных медиапапок',
+    'Visible' => 'Видима',
+    'Show / hide media folders' => 'Показать / скрыть медиапапки',
+    // Grid: сообщения / заголовки
+    'Show' => 'Показать',
+    'Hide' => 'Скрыть',
+    // Grid: сообщения / текст
+    'Media folder - hide' => 'Медиапапка - <b>скрыта</b>.',
+    'Media folder - show' => 'Медиапапка - <b>видима</b>.',
+    // Grid: аудит
+    'media folder with id {0} is hidden' => 'скрытие медиапапки c идентификатором "<b>{0}</b>"',
+    'media folder with id {0} is shown' => 'отображение медиапапки c идентификатором "<b>{0}</b>"',
+
+    // Form
+    '{form.title}' => 'Добавление медиапапки',
+    '{form.appendTo}' => 'Добавление в медиапапку "{name}"',
+    '{form.titleTpl}' => 'Изменение медиапапки "{name}"',
+    // Form: поля
+    'Small icon' => 'Маленький значок',
+    'Medium icon' => 'Средний значок',
+    'Icon CSS' => 'CCS класс значка',
+    'An alias always begins with an "@" symbol' => 'Псевдоним всегда начинается с символа "@"', 
+    'Folder ID, for example: public/uploads' => 'Идентификатор папки (локальный путь), например: public/uploads'
+];
